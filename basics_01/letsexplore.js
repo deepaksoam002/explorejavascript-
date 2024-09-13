c03ec1082b0e4798c6f986cee0b4547c2cced683
deepaksoam002 <165383363+deepaksoam002@users.noreply.github.com>
2024-09-13 17:43:00 +0000

    explore conversion of datatype

--- basics_01/letsexplore.js
+++ basics_01/letsexplore.js
@@ -1,1 +1,129 @@
-console.log(deepak);
+//                    ************ conversion of datatypes ************
+
+/*                     changing number in string , boolean data types 
+let marksinmath = 54
+console.log(typeof marksinmath);
+
+// changing to string 
+let intostring = String (marksinmath);
+console.log(intostring); //  54
+console.log(typeof intostring);  //string
+
+// changing to boolean 
+let intoboolean = Boolean (marksinmath);
+console.log(intoboolean); //true
+console.log(typeof intoboolean);   // boolean
+*/
+
+
+/*                    changing string in to number and boolean data type
+
+let studentName  = "deepak"
+console.log(typeof studentName);
+
+// changing into number
+
+let intonumber = Number (studentName)
+console.log(intonumber); //NaN (not a number)
+console.log(typeof intonumber); // number 
+
+//changing into boolean
+
+let intoboolean = Boolean (studentName)
+console.log(intoboolean); // true
+console.log(typeof intoboolean); // boolean 
+*/
+
+
+/*                    changing boolean in to number and string data type
+
+let islogin  = true
+console.log(typeof islogin);
+
+// changing into number
+
+let intonumber = Number (islogin)
+console.log(intonumber); // 1
+console.log(typeof intonumber); // number 
+
+//changing into string
+
+let intostring = String (islogin)
+console.log(intostring); // writen " true "
+console.log(typeof intostring); // string 
+*/
+
+
+/*                    changing " " in to number,boolean and string data type
+
+let score =" " 
+console.log(typeof score);
+
+// changing into number
+
+let intonumber = Number (score)
+console.log(intonumber); //  => 0
+console.log(typeof intonumber); // number 
+
+//changing into string
+
+let intostring = String (score)
+console.log(intostring); //  just show space 
+console.log(typeof intostring); // string 
+
+//changing into boolean
+
+let intoboolean = Boolean (score)
+console.log(intoboolean); //  true
+console.log(typeof intoboolean); // boolean 
+*/
+
+
+/*                   changing null in to number,boolean and string data type
+
+let score = null
+console.log(typeof score);
+
+// changing into number
+
+let intonumber = Number (score)
+console.log(intonumber); //  => 0
+console.log(typeof intonumber); // number 
+
+//changing into string
+
+let intostring = String (score)
+console.log(intostring); // writen " null "
+console.log(typeof intostring); // string 
+
+//changing into boolean
+
+let intoboolean = Boolean (score)
+console.log(intoboolean); //  false
+console.log(typeof intoboolean); // boolean 
+*/
+
+
+/*                  changing undefined in to number,boolean and string data type
+
+let score = undefined
+console.log(typeof score);
+
+// changing into number
+
+let intonumber = Number (score)
+console.log(intonumber); //  NaN
+console.log(typeof intonumber); // number 
+
+//changing into string
+
+let intostring = String (score)
+console.log(intostring); // writen " undefined "
+console.log(typeof intostring); // string 
+
+//changing into boolean
+
+let intoboolean = Boolean (score)
+console.log(intoboolean); //  false
+console.log(typeof intoboolean); // boolean 
+*/
