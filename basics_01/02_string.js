9e9e0dfe2189a7ea40bf34567d4737e93e0c2d55
deepaksoam002 <165383363+deepaksoam002@users.noreply.github.com>
2024-09-17 05:33:03 +0000

    add string file

---
+++ basics_01/02_string.js
@@ -0,0 +1,35 @@
+//let studentName = new String ("shivamsoam")
+//console.log(studentName);
+
+// lets explore different propert of string 
+//1.   .lenght
+// console.log(studentName.length)  // lenght = 10
+
+ /*2.  .at and charat method 
+
+ let studentName = new String ("shivamsoam")
+console.log(studentName);
+ let index = 4;
+ console.log(`the index of ${index} return the character ${studentName.at(index)}`);
+ console.log(`the index of ${index} return the character ${studentName.charAt(index)}`);
+ */
+
+ /*3. toUppercase and toLowercase
+
+ let studentName = new String ("shivamsoam")
+console.log(studentName.toUpperCase());  //SHIVAMSOAM
+console.log(studentName.toLowerCase());  //shivamsoam
+*/
+
+/* 4. substring and slice and replace
+
+let studentName = new String ("shivamsoam")
+console.log(studentName.substring(0,4));  //shiv
+console.log(studentName.slice(3,6));   //vam
+console.log(studentName.replace("shivam","himanshu"));
+*/
+
+//5. repeat
+
+//const mood = " happy";
+//console.log(`now i am so ${mood.repeat(3)}`);
